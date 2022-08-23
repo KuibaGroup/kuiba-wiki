9a93e4e230c6a6fc833675c35041952d8f8a8b7f
CN-traveler <cn-traveler@outlook.com>
2022-08-23 23:09:35 +0800

    更新插件正则

--- hexo-tag-kuiba-wiki/lib/history.js
+++ hexo-tag-kuiba-wiki/lib/history.js
@@ -4,7 +4,7 @@ module.exports = ctx => function (args, content) {
     const note = ctx.extend.tag.env.extensions.note.fn;
     const code = ctx.extend.tag.env.extensions.code.fn;
     let data = execSync(`git log -p --date=short --pretty=format:"/%cn/%ce/%ad/%s/" ${ctx.source_dir + this.source}`).toString();
-    data = data.replace(/^\/([^\/]+)\/([^\/]+)\/([^\/]+)\/([^\/]+)\/\ndiff --git[^@]+(((?!(\/([^\/]+)\/([^\/]+)\/([^\/]+)\/([^\/]+)\/\ndiff --git[^@]+))[\s\S])*)$/gm, function(match, v1, v2, v3, v4, v5) {
+    data = data.replace(/^\/([^\/]+)\/([^\/]+)\/([^\/]+)\/(.*?)\/\ndiff --git[^@]+(((?!(\/([^\/]+)\/([^\/]+)\/([^\/]+)\/([^\/]+)\/\ndiff --git[^@]+))[\s\S])*)$/gm, function(match, v1, v2, v3, v4, v5) {
         return note(["success", `[${v1}](mailto:${v2} "${v4}")`, v3], code(["lang:diff"], v5));
     });
     return data;
